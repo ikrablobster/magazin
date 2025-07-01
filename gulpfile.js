44afbb1859b74c155510c080bf56baa1766efc12
ikrablobster <ikrablobster@gmail.com>
2025-07-02 02:51:48 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function images() {
 }
 
 function styles(){
-    return src('app/scss/style.scss')
+    return src('app/scss/*.scss')
     .pipe(autoprefixer({
         overrideBrowserslist: ['last 10 versions']
     }))
@@ -67,7 +67,7 @@ function watching(){
             baseDir: 'app/'
         }
     });
-    watch(['app/scss/style.scss'], styles)
+    watch(['app/scss/*.scss'], styles)
     watch(['app/images/src'], images)
     watch(['app/images/sprite'], sprites)
     watch(['app/pages/*', 'app/components/*'], pages)
